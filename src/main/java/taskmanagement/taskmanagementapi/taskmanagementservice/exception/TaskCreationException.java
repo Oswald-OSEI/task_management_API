b80c9c1d3b3abb99bb23e8b89c852b21ce6b5df6
Oswald-OSEI <131367696+Oswald-OSEI@users.noreply.github.com>
2025-01-30 15:39:36 +0000

    feat: add custom exception class for task creation

---
+++ src/main/java/taskmanagement/taskmanagementapi/taskmanagementservice/exception/TaskCreationException.java
@@ -0,0 +1,7 @@
+package taskmanagement.taskmanagementapi.taskmanagementservice.exception;
+
+public class TaskCreationException extends RuntimeException{
+    public TaskCreationException(String message) {
+        super(message);
+    }
+}
