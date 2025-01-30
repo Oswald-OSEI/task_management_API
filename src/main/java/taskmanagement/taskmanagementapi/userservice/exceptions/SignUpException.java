9cf2adf91fdd9d459709293f9f4b79e3c7bd3208
Oswald-OSEI <131367696+Oswald-OSEI@users.noreply.github.com>
2025-01-30 16:05:49 +0000

    feat: add custom exception class for user registration

---
+++ src/main/java/taskmanagement/taskmanagementapi/userservice/exceptions/SignUpException.java
@@ -0,0 +1,7 @@
+package taskmanagement.taskmanagementapi.userservice.exceptions;
+
+public class SignUpException extends RuntimeException{
+    public SignUpException(String message) {
+        super(message);
+    }
+}
