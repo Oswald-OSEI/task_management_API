5eac0953d93bfd2e9a0237f6736f70abdd7c82f7
Oswald-OSEI <131367696+Oswald-OSEI@users.noreply.github.com>
2025-01-30 15:45:53 +0000

    feat: add user not found custom exception class

---
+++ src/main/java/taskmanagement/taskmanagementapi/common/exceptions/UserNotFoundException.java
@@ -0,0 +1,5 @@
+package taskmanagement.taskmanagementapi.common.exceptions;
+
+public class UserNotFoundException extends RuntimeException{
+    public UserNotFoundException(String message) {super(message);}
+}
