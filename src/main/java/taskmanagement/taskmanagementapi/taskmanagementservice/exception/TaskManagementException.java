87b491416b54bf7f98909fc688f30f36ed95039f
Oswald-OSEI <131367696+Oswald-OSEI@users.noreply.github.com>
2025-01-30 16:13:50 +0000

    feat: add custom exception class for task management

---
+++ src/main/java/taskmanagement/taskmanagementapi/taskmanagementservice/exception/TaskManagementException.java
@@ -0,0 +1,7 @@
+package taskmanagement.taskmanagementapi.taskmanagementservice.exception;
+
+public class TaskManagementException extends RuntimeException{
+    public TaskManagementException(String message) {
+        super(message);
+    }
+}
