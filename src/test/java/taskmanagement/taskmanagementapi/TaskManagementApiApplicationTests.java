6c373118de694ab295faa03264579666985bc7ce
Oswald-OSEI <131367696+Oswald-OSEI@users.noreply.github.com>
2025-01-30 16:11:53 +0000

    test: add test application class

---
+++ src/test/java/taskmanagement/taskmanagementapi/TaskManagementApiApplicationTests.java
@@ -0,0 +1,13 @@
+package taskmanagement.taskmanagementapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskManagementApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
