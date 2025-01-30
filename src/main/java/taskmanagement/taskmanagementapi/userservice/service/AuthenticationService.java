134e89ff656b2de179281b6dab2cd52bcffc7bb2
Oswald-OSEI <131367696+Oswald-OSEI@users.noreply.github.com>
2025-01-30 15:17:35 +0000

    feat: add business logic for user login

---
+++ src/main/java/taskmanagement/taskmanagementapi/userservice/service/AuthenticationService.java
@@ -0,0 +1,48 @@
+package taskmanagement.taskmanagementapi.userservice.service;
+
+import lombok.extern.slf4j.Slf4j;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+import taskmanagement.taskmanagementapi.common.security.jwt.TokenService;
+import taskmanagement.taskmanagementapi.userservice.dto.LoginRequestDto;
+import taskmanagement.taskmanagementapi.userservice.dto.LoginResponseDto;
+import taskmanagement.taskmanagementapi.userservice.entity.UserEntity;
+import taskmanagement.taskmanagementapi.common.exceptions.InvalidAuthorizationHeaderException;
+import taskmanagement.taskmanagementapi.userservice.exceptions.InvalidUserCredentialException;
+import taskmanagement.taskmanagementapi.userservice.repository.UserRepository;
+
+@Service
+@Slf4j
+public class AuthenticationService {
+    @Value("${jwt.refresh.secret}")
+    private String refreshSecretString;
+
+    private final TokenService tokenService;
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+
+
+    public AuthenticationService(TokenService tokenService, UserDetailsService userDetailsService, UserRepository userRepository, PasswordEncoder passwordEncoder) {
+        this.tokenService = tokenService;
+        this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    public LoginResponseDto login(LoginRequestDto loginRequestDto){
+        UserEntity user = userRepository.findByUsername(loginRequestDto.username()).orElseThrow(()->new InvalidUserCredentialException("invalid username or password"));
+        if(passwordEncoder.matches(loginRequestDto.password(), user.getPassword())){
+            return new LoginResponseDto(
+                    user.getUsername(),
+                    tokenService.generateAccessToken(user.getId(), user.getUsername()),
+                    tokenService.generateRefreshToken(user.getId(), user.getUsername())
+            );
+        }else{
+            throw new InvalidUserCredentialException("invalid username or password");
+        }
+    }
+
+
+}
